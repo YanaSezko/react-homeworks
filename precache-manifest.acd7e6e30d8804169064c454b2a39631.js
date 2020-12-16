54a25057b8b5fc2090f00baf481e5d4f0ef38701
Yana <33156136+YanaSezko@users.noreply.github.com>
2020-12-16 10:39:16 +0200

    Updates

---
+++ precache-manifest.acd7e6e30d8804169064c454b2a39631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de10b243daf89e0720c140659e6ede5",
+    "url": "/react-homeworks/index.html"
+  },
+  {
+    "revision": "602e85c5a660c8e3f7da",
+    "url": "/react-homeworks/static/css/main.f118cfec.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/react-homeworks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homeworks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602e85c5a660c8e3f7da",
+    "url": "/react-homeworks/static/js/main.26191c53.chunk.js"
+  },
+  {
+    "revision": "87ea755746db0689d010",
+    "url": "/react-homeworks/static/js/runtime-main.8b1af127.js"
+  }
+]);
